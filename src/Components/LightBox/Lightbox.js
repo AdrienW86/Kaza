106361e72306024dde2c0fdb4af777b2c046deee
Adrien <adrien_weiss@outlook.fr>
2022-04-12 13:17:48 +0200

    ajout du display none

--- src/Components/LightBox/Lightbox.js
+++ src/Components/LightBox/Lightbox.js
@@ -5,27 +5,38 @@ import './lightbox.css'
 
 function Lightbox(props) { 
     const [current, setCurrent] = useState(0)
+    const [currentNumber, setCurrentNumber] = useState(1)
     const images = props.images
-   
+
+    let lengthImages = false
     const next = () => {
         if(current === images.length -1) {           
             setCurrent(0)
+            setCurrentNumber(1)
         }else {
             setCurrent(current +1)
+            setCurrentNumber(currentNumber +1)
         }
     }
     const prev = () => {
         if(current === 0) {
             setCurrent(images.length -1)
+            setCurrentNumber(images.length)
         }else{
             setCurrent(current -1)
+            setCurrentNumber(currentNumber -1)
         }  
     }
-    
+        if(images.length > 1) {
+            lengthImages = true
+        }
+    console.log(lengthImages)
   return (
     <>
     <section className='lightbox'>
-        <button onClick={prev} className='prev'>
+        <button
+            onClick={prev} 
+            className= {lengthImages ? 'prev' : 'hidden'}>
             <img className='prev-svg'
                  src={ChevronLeft} 
                  alt="previous"
@@ -34,14 +45,18 @@ function Lightbox(props) {
             <img className='photo-lightbox'                        
                  src={images[current]}               
                  alt='lightbox'
-            />  
-        <button onClick={next} className='next' >
+            />   
+        <button 
+            onClick={next} 
+            className= {lengthImages ? 'next' : 'hidden'}>
             <img className='next-svg'  
                  src={ChevronRight} 
                  alt="next">
             </img> 
-        </button>                      
+        </button> 
+                             
     </section>
+    <div className='number-picture'> {currentNumber}/{images.length}</div>
     </>
   )
 }
